0f4b1cc66da585a106b6cfbaccfe05ec9da41f90
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-03-13 21:53:56 -0700

    tabs focus interactivity with left and right arrowkeys, allows the index to circle around

--- space-travel-website/tabs.js
+++ space-travel-website/tabs.js
@@ -1,13 +1,37 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]')
 
-tabList.addEventListener('keydown', (e) => {
+let countIndex = 0;
+
+tabList.addEventListener('keydown', changeTabIndex)
+
+function changeTabIndex(e) {
   const keydownLeft = 37;
   const keydownRight = 39;
 
+  console.log(countIndex);
+
   //Change the tabindex of the current tab to -1
-  
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabs[countIndex].setAttribute("tabindex", -1);
+  }
   //if the right key is pushed, move to the next tab on the right
 
+  if (e.keyCode === keydownRight) {
+    countIndex++;
+    if (countIndex >= tabs.length) {
+      countIndex = 0;
+    }
+  }
+
   //if the left key is pushed, move to the next tab on the left
-})
+  if(e.keyCode == keydownLeft) {
+    countIndex--;
+    if (countIndex < 0) {
+      countIndex = tabs.length - 1;
+    }
+  }
+
+  tabs[countIndex].setAttribute("tabindex", 1)
+  tabs[countIndex].focus();
+}
