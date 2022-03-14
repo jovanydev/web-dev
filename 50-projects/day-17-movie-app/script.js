10316430d6790935f33f3081664e25371acaf346
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-03-14 14:41:51 -0700

    fetch movie data, function showMovies, style the movie elements, function getClassByRate

--- 50-projects/day-17-movie-app/script.js
+++ 50-projects/day-17-movie-app/script.js
@@ -1,3 +1,4 @@
+const main = document.getElementById('main');
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
@@ -11,7 +12,39 @@ getMovies(API_URL);
 function getMovies(url) {
   fetch(url)
   .then((resp) => resp.json())
-  .then(data => console.log(data))
+  .then(data => showMovies(data.results))
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const {title, poster_path, vote_average, overview} = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML= 
+    `
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        <p>${overview}</p>
+      </div>
+    `
+
+    main.appendChild(movieEl);
+  })
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) return 'green';
+  if (vote >= 5) return 'orange';
+  return 'red';
 }
 
 form.addEventListener('submit', (e) => {
